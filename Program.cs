f6b45c073cf52bee056ddb9af4d2c6cc0f34081a
HocChr <52623693+HocChr@users.noreply.github.com>
2020-05-27 15:01:09 +0200

    Add files via upload
    Initialer Setup. StockEntity hinzugefügt

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockCheckerII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
