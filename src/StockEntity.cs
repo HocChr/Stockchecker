9e432f13e9c0278fd343ac77ed90bc6f85148900
Hochheim Christian <chochheim@pb-group.de>
2020-05-27 15:08:10 +0200

    Vervollständige den initialen Setup

---
+++ src/StockEntity.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+namespace StockCheckerII
+{
+    internal class StockEntity
+    {
+        List<YearDataSet> _yearData = new List<YearDataSet>();
+
+        public enum Rate
+        {
+            A,
+            B,
+            C
+        }
+
+        public class YearDataSet
+        {
+            public double Dividend { get; set; }
+            public double Earning { get; set; }
+            public int Year { get; set; }
+        }
+
+        public Rate Rating { get; set; }
+        public string Name { get; set; }
+        public double EarningCorrelation { get; set; }
+        public double EarningGrowthTreeYears { get; set; }
+        public double EarningGrowthLastYear{ get; set; }
+        public double PayoutRatio{ get; set; }
+        internal List<YearDataSet> YearData
+        {
+            get
+            {
+                _yearData.Sort((x, y) => x.Year.CompareTo(y.Year));
+                return _yearData;
+            }
+
+            set
+            {
+                _yearData = value;
+                _yearData.Sort((x, y) => x.Year.CompareTo(y.Year));
+            }
+        }
+    }
+}
